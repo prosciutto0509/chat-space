790aa6786bd81c04b2362a70255f573218aaf9f0
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-06-18 14:59:23 +0900

    finish_

---
+++ app/assets/javascript/modules/message.js
@@ -0,0 +1,65 @@
+function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="MessageBox" data-message-id=${message.id}>
+          <div class="MessageInfo">
+            <div class="MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox" data-message-id=${message.id}>
+        <div class="MessageInfo">
+          <div class="MessageInfo__userName">
+            ${message.user_name}
+          </div>
+          <div class="MessageInfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+$(function(){
+ $('.form_box').on('submit', function(e){
+  e.preventDefault();
+  let formData = new FormData(this);
+  let url = $(this).attr('action');
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+})
+.done(function(data){
+  let html = buildHTML(data);
+  $('.main_chat').append(html);
+  $('form')[0].reset();
+  $('.main_chat').animate({ scrollTop: $('.main_chat')[0].scrollHeight});
+  $('.send').prop('disabled', false);
+})
+.fail(function() {
+  alert("メッセージ送信に失敗しました");
+   });
+  });
+});
