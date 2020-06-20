07eb08c23fa0853b9c47913cfd2c7bc85cf21fcf
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-06-20 16:34:23 +0900

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:prosciutto0509/chatp-space.git'
+set :repo_url,  'git@github.com:prosciutto0509/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
